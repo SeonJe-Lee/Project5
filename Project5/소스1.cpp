a4c9da5b1fae4cfeeb06553018520fb136e086ad
Seonje-Lee <tjswp21@cu.ac.kr>
2019-05-24 11:49:01 +0900

    5/24

--- Project5/소스1.cpp
+++ Project5/소스1.cpp
@@ -1,89 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define Q_SIZE 4
+#define cQ_SIZE 4
 
 typedef char element;
 typedef struct {
-	element queue[Q_SIZE];
+	element queue[cQ_SIZE];
 	int front, rear;
 } QueueType;
 
-
 QueueType *createQueue() {
-	QueueType *Q;
-	Q = (QueueType *)malloc(sizeof(QueueType));
-	Q->front = -1;
-	Q->rear = -1;
-	return Q;
+	QueueType *cQ;
+	cQ = (QueueType *)malloc(sizeof(QueueType));
+	cQ->front = 0;
+	cQ->rear = 0;
+	return cQ;
 }
 
 
-int isEmpty(QueueType *Q) {
-	if (Q->front == Q->rear) {
-		printf("Queue is empty! ");
+int isEmpty(QueueType *cQ) {
+	if (cQ->front == cQ->rear) {
+		printf("Circular Queue is empty! ");
 		return 1;
 	}
 	else return 0;
 }
 
 
-int isFull(QueueType *Q) {
-	if (Q->rear == Q_SIZE - 1) {
-		printf(" Queue is full! ");
+int isFull(QueueType *cQ) {
+	if (((cQ->rear + 1) % cQ_SIZE) == cQ->front) {
+		printf(" Circular Queue is full! ");
 		return 1;
 	}
 	else return 0;
 }
 
 
-void enQueue(QueueType *Q, element item) {
-	if (isFull(Q)) return;
+void enQueue(QueueType *cQ, element item) {
+	if (isFull(cQ)) return;
 	else {
-		Q->rear++;
-		Q->queue[Q->rear] = item;
+		cQ->rear = (cQ->rear + 1) % cQ_SIZE;
+		cQ->queue[cQ->rear] = item;
 	}
 }
 
 
-element deQueue(QueueType *Q) {
-	if (isEmpty(Q)) return NULL;
+element deQueue(QueueType *cQ) {
+	if (isEmpty(cQ)) exit(1);
 	else {
-		Q->front++;
-		return Q->queue[Q->front];
+		cQ->front = (cQ->front + 1) % cQ_SIZE;
+		return cQ->queue[cQ->front];
 	}
 }
 
 
-element peek(QueueType *Q) {
-	if (isEmpty(Q)) exit(1);
-	else return Q->queue[Q->front + 1];
+element peek(QueueType *cQ) {
+	if (isEmpty(cQ)) exit(1);
+	else return cQ->queue[(cQ->front + 1) % cQ_SIZE];
 }
 
 
-void printQ(QueueType *Q) {
-	int i;
-	printf(" Queue : [");
-	for (i = Q->front + 1; i <= Q->rear; i++)
-		printf("%3c", Q->queue[i]);
-	printf(" ]");
+void printQ(QueueType *cQ) {
+	int i, first, last;
+	first = (cQ->front + 1) % cQ_SIZE;
+	last = (cQ->rear + 1) % cQ_SIZE;
+	printf(" Circular Queue : [");
+	i = first;
+	while (i != last) {
+		printf("%3c", cQ->queue[i]);
+		i = (i + 1) % cQ_SIZE;
+	}
+	printf(" ] ");
 }
 
 void main(void) {
-	QueueType *Q1 = createQueue();
+	QueueType *cQ = createQueue();
 	element data;
-	printf("\n ***** ¼øÂ÷ Å¥ ¿¬»ê ***** \n");
-	printf("\n »ðÀÔ A>>"); enQueue(Q1, 'A'); printQ(Q1);
-	printf("\n »ðÀÔ B>>"); enQueue(Q1, 'B'); printQ(Q1);
-	printf("\n »ðÀÔ C>>"); enQueue(Q1, 'C'); printQ(Q1);
-	data = peek(Q1);  printf(" peek item : %c \n", data);
-	printf("\n »èÁ¦ >>"); data = deQueue(Q1); printQ(Q1);
+	printf("\n ***** ¿øÇü Å¥ ¿¬»ê ***** \n");
+	printf("\n »ðÀÔ A>>"); enQueue(cQ, 'A'); printQ(cQ);
+	printf("\n »ðÀÔ B>>"); enQueue(cQ, 'B'); printQ(cQ);
+	printf("\n »ðÀÔ C>>"); enQueue(cQ, 'C'); printQ(cQ);
+	data = peek(cQ);       printf(" peek item : %c \n", data);
+	printf("\n »èÁ¦ >>"); data = deQueue(cQ); printQ(cQ);
 	printf("\t »èÁ¦ µ¥ÀÌÅÍ : %c", data);
-	printf("\n »èÁ¦ >>"); data = deQueue(Q1); printQ(Q1);
-	printf("\t »èÁ¦ µ¥ÀÌÅÍ : %c", data);
-	printf("\n »èÁ¦ >>"); data = deQueue(Q1); printQ(Q1);
+	printf("\n »èÁ¦ >>"); data = deQueue(cQ); printQ(cQ);
 	printf("\t\t »èÁ¦ µ¥ÀÌÅÍ : %c", data);
-
-	printf("\n »ðÀÔ D>>"); enQueue(Q1, 'D'); printQ(Q1);
-	printf("\n »ðÀÔ E>>"); enQueue(Q1, 'E'); printQ(Q1);
+	printf("\n »èÁ¦ >>"); data = deQueue(cQ); printQ(cQ);
+	printf("\t\t »èÁ¦ µ¥ÀÌÅÍ : %c", data);
+	printf("\n »ðÀÔ D>>"); enQueue(cQ, 'D'); printQ(cQ);
+	printf("\n »ðÀÔ E>>"); enQueue(cQ, 'E'); printQ(cQ);
 	getchar();
 }
